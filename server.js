cda32df34c058cc09f27f5ba7aa6fc44cb212aaf
taylor d <taylor.delph@eyecuelab.com>
2021-10-25 14:59:11 -0700

    41. Connect MongoDB to app

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
-app.get('/', (req, res)=> res.send({msg: 'Welcome to the ContactKeeper API...'}))
+//Connect Database 
+connectDB();
+
+app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API...'}))
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
@@ -10,4 +15,4 @@ app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
