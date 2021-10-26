5e008832a97e887d85b35f67112273a7cdca4f07
taylor d <taylor.delph@eyecuelab.com>
2021-10-26 15:51:35 -0700

    47. Contact model & get contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,24 @@
 // CRUD functionality. Specific to User
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route     GET api/contacts
 // @descrip   Get all user's contacts
 // @access    Private 
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async  (req, res) => {
+  try {
+    // contacts have a user field which is an object id. We want to get contacts for this specific user and since we're using auth middleware we have access to req.user object. Sort by -1 means most recent contacts first
+    const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
 });
 
 // @route     POST api/contacts
