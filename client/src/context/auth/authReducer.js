1bde31178c4a407b280d213977e1e6f251fcc924
taylor d <taylor.delph@eyecuelab.com>
2021-11-14 09:40:21 -0800

    load user & set token

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -12,6 +12,13 @@ import {
 
 export default (state, action) => {
   switch(action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload
+      }
     case REGISTER_SUCCESS:
       //if there's register success want to put token we get back inside local storage
       localStorage.setItem('token', action.payload.token);
@@ -23,6 +30,7 @@ export default (state, action) => {
       };
     // if it fails we want to remove token from storage
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
