4eda2677073d211e2e1fddc3ef177d65616ac9fe
taylor d <taylor.delph@eyecuelab.com>
2021-11-14 12:03:15 -0800

    69. Get & Clear Contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,32 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
+import Spinner from '../layout/Spinner';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
   const nodeRef = React.useRef(null)
   const contactContext = useContext(ContactContext);
 
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
 
-  if(contacts.length === 0) {
+  useEffect(() => {
+    getContacts();
+    // esling-disable-next-line  
+  }, []);
+
+  if(contacts != null && contacts.length === 0 && !loading) {
     return <h4>Please add a contact</h4>
   }
   return (
     <>
-      <TransitionGroup>
-        {filtered !== null 
-          ? filtered.map(contact => (
-            <CSSTransition nodeRef={nodeRef} key={contact._id} timeout={550} classNames="item">
-              <ContactItem contact={contact}/> 
-            </CSSTransition>
-            )) 
-          : contacts.map(contact => (
-            <CSSTransition nodeRef={nodeRef} key={contact._id} timeout={550} classNames="item" >
-              <ContactItem contact={contact}/>
-            </CSSTransition>
-          ))}
+      {contacts !== null && !loading ? (
+        <TransitionGroup>
+          {filtered !== null 
+            ? filtered.map(contact => (
+                <CSSTransition 
+                  nodeRef={nodeRef} 
+                  key={contact._id} 
+                  timeout={550} 
+                  classNames="item"
+                >
+                  <ContactItem contact={contact}/> 
+                </CSSTransition>
+              )) 
+            : contacts.map(contact => (
+                <CSSTransition 
+                  nodeRef={nodeRef} 
+                  key={contact._id} 
+                  timeout={550} 
+                  classNames="item" 
+                >
+                  <ContactItem contact={contact}/>
+                </CSSTransition>
+            ))
+          }
       </TransitionGroup>
+      ) : <Spinner />}
     </>
   );
 };
